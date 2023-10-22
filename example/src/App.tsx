f0ad69d08d12612e6b9b689a29ce6502b5b2e2bc
Pyae Sone Htet <pyaesonehtet432@gmail.com>
2023-10-22 14:07:04 +0630

    feat: add currency and account number format function

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-currency_formater';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import {
+  formatCurrencyInput,
+  formatCurrency,
+  formatAccountNumber,
+} from 'react-native-currency_formater';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [value, setValue] = React.useState<string>('');
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  React.useEffect(() => {}, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>{formatCurrency('0.2333')}</Text>
+      <Text>{formatAccountNumber(3443435324535543)}</Text>
+      <TextInput
+        value={value}
+        onChangeText={(_value) => setValue(formatCurrencyInput(_value, 3))}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -27,5 +35,13 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 50,
+    marginHorizontal: 20,
+    padding: 10,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,36 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+/* eslint-disable no-useless-escape */
+export function formatCurrencyInput(
+  inputValue: string,
+  decimalNum: number
+): string {
+  let isDecimalExists = decimalNum === 0 ? '' : '.';
+  let removeCommaAndOnlyDigit: string = inputValue
+    .replace(/[^\d\.]/gi, '')
+    .replace(/,/g, '');
+  let removeLeadingZeroAndDot: string = removeCommaAndOnlyDigit
+    .replace(/^0+/, '')
+    .replace(/^\./, '');
+  let splitDotArr: any = removeLeadingZeroAndDot.split('.');
+  let decimal: string = '';
+  if (splitDotArr.length > 1 && splitDotArr[1].length > decimalNum) {
+    decimal = isDecimalExists + splitDotArr[1].substring(0, decimalNum);
+  } else {
+    decimal = splitDotArr.length > 1 ? isDecimalExists + splitDotArr[1] : '';
+  }
+  let result = splitDotArr[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return result + decimal;
+}
+
+export function formatCurrency(amount: string): string {
+  return Number(amount)
+    .toFixed(2)
+    .replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+export function formatAccountNumber(digit: number): string {
+  let spacingDigit: RegExpMatchArray | null = digit.toString().match(/.{1,4}/g);
+  if (!spacingDigit) {
+    return '';
+  }
+  return spacingDigit.join('   ');
 }
